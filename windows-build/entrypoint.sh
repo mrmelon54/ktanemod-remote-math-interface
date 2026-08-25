775f620122cc3feabbe398db20aeee1667a33fa9
Melon <git@mrmelon54.com>
2026-08-25 02:23:35 +0100

    Revert "Implement compiling for Windows arm and arm64"
    This reverts commit a88121827b760abefdb7c6a146c5999ff7b88d2c.

--- windows-build/entrypoint.sh
+++ windows-build/entrypoint.sh
@@ -3,5 +3,3 @@ export CGO_ENABLED=1
 export GOOS=windows
 GOARCH=amd64 CC=x86_64-w64-mingw32-gcc go build -buildvcs=false -o dist/ktanemod-remote-math-interface-windows-amd64.dll -buildmode=c-shared .
 GOARCH=386 CC=i686-w64-mingw32-gcc go build -buildvcs=false -o dist/ktanemod-remote-math-interface-windows-386.dll -buildmode=c-shared .
-GOARCH=arm64 CC=arm64ec-w64-mingw32-gcc go build -buildvcs=false -o dist/ktanemod-remote-math-interface-windows-arm64.dll -buildmode=c-shared .
-GOARCH=arm CC=armv7-w64-mingw32-gcc go build -buildvcs=false -o dist/ktanemod-remote-math-interface-windows-arm.dll -buildmode=c-shared .
